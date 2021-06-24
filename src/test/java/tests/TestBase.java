f53a4e1b1fb50b46ebaa11bd0ee215d58b058303
mhorbachov <mhorbachov@playtika.com>
2021-06-24 13:36:13 +0300

    Lesson #9: added Allure attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -10,12 +14,16 @@ public class TestBase {
 
     @BeforeAll
     public static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
     public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
         closeWebDriver();
     }
 
