3e2c1dffef27cb800fa9b901e14833809e1e80ca
mhorbachov <mhorbachov@playtika.com>
2021-06-24 12:40:53 +0300

    Lesson #9: added configuration for Selenoid support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,22 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     public static void setup() {
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
     }
 
 }
